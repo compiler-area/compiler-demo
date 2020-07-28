38e0978af0236b2db149e537e6b9ba6b76182477
compiler-area <area.compiler@gmail.com>
2020-07-28 02:19:43 +0200

    second time

--- js/style.js
+++ js/style.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
    $(window).scroll(function(){
         if($(window).scrollTop() > $(window).height()){
-            $(".fixed-top").css({"padding":"15px"});
-            $(".fixed-top").addClass("bg-white").removeClass("nav-trans");
+            $(".fixed-top").css({"padding":"15px" , "box-shadow" : "3px 19px 102px 1px #000"});
+            $(".fixed-top").addClass("bg-white navbar-light").removeClass("nav-trans navbar-dark");
 
         }
         else{
-            $(".fixed-top").css({"padding":"30px"});
-            $(".fixed-top").removeClass("bg-white").addClass("nav-trans");
+            $(".fixed-top").css({"padding":"30px" , "box-shadow" : "none"});
+            $(".fixed-top").removeClass("bg-white navbar-light").addClass("nav-trans navbar-dark");
         }
     });
 });
